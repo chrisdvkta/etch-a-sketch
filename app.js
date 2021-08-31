3610321a185a0571efb0443c1326b6063ede79fc
Krish Devkota <chrisdevkota77@gmail.com>
2021-08-31 18:01:39 +0545

    Fill n clear

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const container = document.querySelector('#container');
 let rows = document.getElementsByClassName('grid-row');
 let cells = document.getElementsByClassName('grid-column');
+const reset = document.getElementById("reset")
 
 defaultGrid();
-
 
 function defaultGrid(){
     createRow(18);
@@ -22,9 +22,20 @@ function createColumn(cellNum){
         for (let j =0;j<cellNum;j++){
             let newCell = document.createElement('div');
             rows[j].appendChild(newCell).className= 'grid-column';
+            // newCell.addEventListener('mouseover',function(){newCell.classList.add('fill');});
+            reset.addEventListener('click',()=>{newCell.classList.remove('fill');})
         }
     }
 
 }
+
+container.addEventListener("mouseover",function(e) {        //event listener delegattion from container and test the target.
+    const tgt = e.target;
+    console.log(tgt);
+    if (tgt.classList.contains("grid-column")) tgt.classList.add("fill");
+});
+
+
+
 
 
