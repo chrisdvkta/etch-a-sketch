000f203150c7550c71a44973c5ab345afa3615a2
chrisdvkta <chrisdevkota77@gmail.com>
2023-03-10 12:48:47 +0545

    Grid creation inside container

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
-const container = document.querySelector('#container');
-let rows = document.getElementsByClassName('grid-row');
-let cells = document.getElementsByClassName('grid-column');
-const reset = document.getElementById("reset")
+const grid = document.querySelector('.grid');
+let gridValue = document.querySelector('.grid-size');
+let gridSize = document.querySelector('input');
+const resetBtn = document.querySelector('.reset');
+const applyGridSize = document.querySelector('.apply');
+let squareSize = 8;
 
-defaultGrid();
+createGrid(squareSize);
 
-function defaultGrid(){
-    createRow(18);
-    createColumn(18);
+function createDiv(size){
+  const div = document.createElement('div');
+  div.classList.add('box');
+  div.style.width = `${size}px`;
+  div.style.height = `${size}px`;
+
+  return div;
 }
 
-function createRow(rowNum){
-    for (let i = 0; i<rowNum;i++){
-        let row = document.createElement('div');
-        container.appendChild(row).className= 'grid-row';
+
+function createGrid(gridSize){
+  for (let i = 0; i<gridSize;i++){
+    for (let j =0; j<gridSize;j++){
+      grid.appendChild(createDiv(grid.clientWidth/gridSize));
     }
+  }
 }
 
-function createColumn(cellNum){
-    for (let i = 0; i<rows.length; i++){
-        for (let j =0;j<cellNum;j++){
-            let newCell = document.createElement('div');
-            rows[j].appendChild(newCell).className= 'grid-column';
-            // newCell.addEventListener('mouseover',function(){newCell.classList.add('fill');});
-            reset.addEventListener('click',()=>{newCell.classList.remove('fill');})
-        }
-    }
-
+function reset(){
+  while (grid.lastChild){
+    grid.removeChild(grid.lastChild);
+  }
+  createGrid(squareSize);
 }
 
-container.addEventListener("mouseover",function(e) {        //event listener delegattion from container and test the target.
-    const tgt = e.target;
-    console.log(tgt);
-    if (tgt.classList.contains("grid-column")) tgt.classList.add("fill");
+grid.addEventListener('mouseover',function (e){
+  if(e.target.matches('.box')) 
+  {e.target.classList.add("active")};
+
+})
+
+gridSize.addEventListener('input',function (e){
+  squareSize = e.target.value;
+  gridValue.textContent = `${squareSize}x ${squareSize}`;
+})
+
+applyGridSize.addEventListener('click', function (){
+  reset();
 });
 
-
-
-
-
+resetBtn.addEventListener('click', reset);
